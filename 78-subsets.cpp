9ff504aba7c69dce7cdc202967146cdc595d38e8
cyber-shah <shah.pranc@northeastern.edu>
2024-02-24 20:01:55 -0500

    beats 100%

--- 78-subsets.cpp
+++ 78-subsets.cpp
@@ -1,49 +1,45 @@
-#include <set>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
   vector<vector<int>> subsets(vector<int> &nums) {
-    set<set<int>> result;
-    set<int> empty = {};
-    // append this empty set to the result
-    result.insert(empty);
-
-    // build a iterator for the for loop
-    set<set<int>>::iterator result_itr;
-
-    // for each item in the result now, append the nums to it
-    // and decide based on DECISION
-    for (result_itr = result.begin(); result_itr != result.end();
-         result_itr++) {                      // O(2^n)
-      for (int j = 0; j < nums.size(); j++) { // O(n)
-        build(result_itr, nums[j], &result);
-      }
-    }
-
-    // convert the set of sets to vector of vectors
     vector<vector<int>> result_vector;
-    for (result_itr = result.begin(); result_itr != result.end();
-         result_itr++) {
-      result_vector.push_back(
-          vector<int>(result_itr->begin(), result_itr->end()));
+
+    // Initially, add an empty set to the result
+    result_vector.push_back(vector<int>());
+
+    // Iterate through each element in nums
+    for (int j = 0; j < nums.size(); j++) {
+      // Get the current size of result_vector before modification
+      int n = result_vector.size();
+
+      // Iterate over existing subsets and append num to create new subsets
+      for (int i = 0; i < n; i++) {
+        // copy the exisiting set
+        vector<int> new_set = result_vector[i];
+        // add the current number to the new set
+        new_set.push_back(nums[j]);
+        // add the new set to the result
+        result_vector.push_back(new_set);
+      }
     }
 
     return result_vector;
   }
 
-  void build(set<set<int>>::iterator result_itr, int num,
-             set<set<int>> *result) {
+  void
+  build(vector<int> *result_itr, int num,
+        vector<vector<int>> *result_vector) { // gets a pointer to the result
     // two choices :
-    // 1. include the number in the set
-    set<int> new_set = *result_itr;
-    new_set.insert(num);
+    // 1. include the number in the new_set
+    // instead of creating copies, we can directly modify the result set
+    // TODO: learn what this means more on pointers
+    vector<int> new_set = *result_itr;
+    new_set.push_back(num);
 
     // 2. don't include the number
     // already included in the result
-
-    // modify the result set
-    result->insert(new_set);
+    (*result_vector).push_back(new_set);
   }
 };
