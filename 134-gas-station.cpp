07cd65460795b85ef887bf83d5606942aa1a6389
cyber-shah <shah.pranc@northeastern.edu>
2024-03-09 20:33:25 -0500

    done

--- 134-gas-station.cpp
+++ 134-gas-station.cpp
@@ -1,62 +1,31 @@
+
 #include <iostream>
-#include <set>
 #include <vector>
+
 using namespace std;
 
 class Solution {
 public:
   int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    int totalGas = 0;
+    int totalCost = 0;
+    int currentGas = 0;
     int start = 0;
-    int gasRemaning = gas[start];
-    int currentStop = 0;
-    bool solutionFound = false;
 
-    std::set<int> visitedSet;
-    visitedSet.insert(start);
+    for (int i = 0; i < gas.size(); ++i) {
+      totalGas += gas[i];
+      totalCost += cost[i];
+      currentGas += gas[i] - cost[i];
 
-    int n = gas.size();
-
-    while (visitedSet.size() < n) {
-      cout << "visited so far : " << visitedSet.size() << endl;
-      cout << "start : " << start << endl;
-      cout << "current Stop :" << currentStop << endl;
-      cout << "gas Remaning :" << gasRemaning << endl;
-      cout << "next move cost : " << cost[currentStop] << endl;
-      cout << "gas remaning if we move : " << gasRemaning - cost[currentStop]
-           << endl;
-      cout << " " << endl;
-
-      // 1. check if we can move ahead
-      if (gasRemaning - cost[currentStop] > 0) {
-        // move from the current stop, and pay the gas
-        gasRemaning -= cost[currentStop];
-        // fill gas at the next stop
-
-        currentStop++;
-        currentStop = currentStop % gas.size();
-        gasRemaning += gas[currentStop];
-        // make currentStop loop in that range
-      }
-
-      // if that doesn't work out, start from a new point which
-      // is not between old start and current stop
-      else {
-        start += 1;
-        currentStop = start;
-        gasRemaning = gas[start];
-
-        visitedSet.insert(currentStop);
-      }
-
-      // check if we have already arrived
-      // and found the solution
-      if (currentStop - 1 == start && visitedSet.size() == n) {
-        solutionFound = true;
-        break;
+      // If currentGas becomes negative, reset the starting station
+      if (currentGas < 0) {
+        currentGas = 0;
+        start = i + 1;
       }
     }
 
-    if (solutionFound) {
+    // Check if there is a solution
+    if (totalGas >= totalCost) {
       return start;
     } else {
       return -1;
