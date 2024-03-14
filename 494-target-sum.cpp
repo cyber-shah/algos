36d376f51818f8ca1f520f90fae20405d8cdf827
cyber-shah <shahcodes@proton.me>
2024-03-13 22:30:46 -0400

    converting dp

--- 494-target-sum.cpp
+++ 494-target-sum.cpp
@@ -5,14 +5,18 @@ using namespace std;
 class Solution {
 public:
   int output = 0;
+  vector<vector<int>> *dp;
   int findTargetSumWays(vector<int> &nums, int target) {
+    vector<vector<int>> actualvector(nums.size(), vector<int>(nums.size(), -1));
+    dp = &actualvector;
+
     recursiveSum(nums, target, 0, 0);
     return output;
   }
 
   void recursiveSum(vector<int> &nums, int target, int total, int index) {
     // base case
-    if (index == nums.size() - 1) {
+    if (index == nums.size()) {
       if (total == target) {
         ++output;
       } else {
@@ -20,12 +24,15 @@ public:
       }
     }
 
-    // choice 1
-    int leftTotal = total + nums[index];
-    recursiveSum(nums, target, leftTotal, index + 1);
+    if (index < nums.size()) {
 
-    // choice 2
-    int rightTotal = total - nums[index];
-    recursiveSum(nums, target, rightTotal, index + 1);
+      // choice 1
+      int leftTotal = total + nums[index];
+      recursiveSum(nums, target, leftTotal, index + 1);
+
+      // choice 2
+      int rightTotal = total - nums[index];
+      recursiveSum(nums, target, rightTotal, index + 1);
+    }
   }
 };
